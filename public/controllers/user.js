52c38aee68ca73668cfde62558974eb13d8a145b
BitB Warren Gonsalves <bitbwarren@BitBs-MacBook-Pro.local>
2015-02-16 09:57:47 +0530

    temporary commit

--- public/controllers/user.js
+++ public/controllers/user.js
@@ -1,40 +1,43 @@
 angular.module('ForChange')
   .controller('UserCtrl', function($scope, $alert, $location, $http, $rootScope, User, $routeParams) {
   	User.get({
-	  		uslug : $routeParams.uslug
-	  	},
-	  	function (user) {
-	  		console.log(user);
-	  			$scope.user = user;
-	  	},function(err){
-	      $location.path('/')
-	      $alert({
-	        content: 'No such User',
-	        placement: 'right',
-	        type: 'danger',
-	        duration: 5
-	      });
-    	});
-  // 	$scope.user = {
-  // 		"firstname":"Warren",
-  // 		"lastname":"Gonsalves",
-  // 		"location":"Mumbai",
-  // 		"joinDate":"21 Jan 2015",
-  // 		"anon":true,
-  // 		"type":"admin",
-  // 		"complaints":[
-  // 			{"title":"test"},
-  // 			{"desc":"asd asd asd asd asd asd asd asd asd asd asd asd asd as "},
-  // 			{"location":"Mumbai"},
-  // 			{"cat":"Utilities"},
-  // 			{"subCat":"Roads"},
-  // 			{"date":"30 Jan 2015"}
-  // 		]
-		// };
+  		uslug : $routeParams.uslug
+  	},
+  	function (user) {
+  		console.log(user);
+  			$scope.user = user;
+  	},function(err){
+      $location.path('/')
+      $alert({
+        content: 'No such User',
+        placement: 'right',
+        type: 'danger',
+        duration: 5
+      });
+  	});
 
+    //promote a person to staff
 		$scope.makeStaff = function () {
 			console.log('hihihi');
 			//call to make staff
-		}
+		};
 
-});
+
+    //user is viewing his/her own page
+    if ($rootScope.currentUser.profile.slug == $routeParams.uslug) 
+      $scope.isCurrUser = true;
+    else $scope.isCurrUser = false;
+    
+
+    //change password
+    $scope.password = {};
+    $scope.same = true;
+    $scope.isSame = function () {
+      if($scope.password.new == $scope.password.newConf)
+       $scope.same = true;
+      else $scope.same = false;
+    }
+    $scope.changePass = function () {
+      //call change pass
+    }
+  });
